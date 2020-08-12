99df0055fe4161431a102a61cd4ff61244b45fc4
nikolaki333 <nurlicic@gmail.com>
2020-08-12 11:46:44 +0200

    refactored into separate modules

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,10 @@
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-const Blog = require('./models/blog');
 const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-app.get('/', (request, response) => {
-	response.send('<h1>test</h1>');
-});
-
-app.get('/api/blogs', (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
-});
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body);
-
-	blog.save().then((result) => {
-		response.status(201).json(result);
-	});
-});
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
 	console.log(`Server running on port ${config.PORT}`);
 });
--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
-const config = require('../utils/config');
 
-const url = config.MONGODB_URI;
-mongoose
-	.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then((result) => {
-		console.log('Connected to MongoDB');
-	})
-	.catch((err) => {
-		console.log('Error connecting to MongoDB', err.message);
-	});
+mongoose.set('useFindAndModify', false);
 
 const blogSchema = mongoose.Schema({
 	title: String,
